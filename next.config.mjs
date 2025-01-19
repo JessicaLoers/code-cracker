acd9124e9df30dc3539d1cba00b6c87748c7e581
Jessica Lörs <jessica.loers@gmail.com>
2025-01-19 22:37:06 +0100

    fix build process issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ import nextra from 'nextra';
 const withNextra = nextra({
   // ... Other Nextra config options
   latex: true,
-  // search: {
-  //   codeblocks: false,
-  // },
-  // contentDirBasePath: '/docs',
 });
 
 // You can include other Next.js configuration options here, in addition to Nextra settings:
--- src/app/_meta.global.js
+++ src/app/_meta.global.js
@@ -8,31 +8,8 @@ const metaGlobal = {
       bubble: {
         title: 'Bubble Sort',
       },
-      // selection: {
-      //   title: 'Selection Sort',
-      // },
-      // insertion: {
-      //   title: 'Insertion Sort',
-      // },
-      // merge: {
-      //   title: 'Merge Sort',
-      // },
-      // quick: {
-      //   title: 'Quick Sort',
-      // },
     },
   },
-  // docs: {
-  //   title: 'Documentation',
-  //   items: {
-  //     index: {
-  //       title: 'Getting Started',
-  //     },
-  //     advanced: {
-  //       title: 'Advanced Topics',
-  //     },
-  //   },
-  // },
 };
 
 export default metaGlobal;
